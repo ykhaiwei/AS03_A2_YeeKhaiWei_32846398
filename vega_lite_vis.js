76bba19280e9fbf0840d800988de66ff96a865a9
Khai Wei <121887080+ykhaiwei@users.noreply.github.com>
2024-10-10 00:01:05 +0800

    Add visualization files and supporting documents

---
+++ vega_lite_vis.js
@@ -0,0 +1,41 @@
+var vg_1 = "choropleth_map.vg.json";
+vegaEmbed("#choropleth_map", vg_1, { actions: false })
+  .then(function (result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+  })
+  .catch(console.error);
+
+var vg_2 = "bubble_plot.vg.json";
+vegaEmbed("#bubble_plot", vg_2, { actions: false })
+  .then(function (result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+  })
+  .catch(console.error);
+
+var vg_3 = "bump_chart.vg.json";
+vegaEmbed("#bump_chart", vg_3, { actions: false })
+  .then(function (result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+  })
+  .catch(console.error);
+
+var vg_4 = "line_chart.vg.json";
+vegaEmbed("#line_chart", vg_4, { actions: false })
+  .then(function (result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+  })
+  .catch(console.error);
+
+var vg_5 = "stacked_chart.vg.json";
+vegaEmbed("#stacked_chart", vg_5, { actions: false })
+  .then(function (result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+  })
+  .catch(console.error);
+
+var vg_6 = "alluvial_diagram.vg.json";
+vegaEmbed("#alluvial_diagram", vg_6, { actions: false })
+  .then(function (result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+  })
+  .catch(console.error);
